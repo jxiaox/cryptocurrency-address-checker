e1960379de16ca70b7748173a9bde14fd078a7b7
jxiaox <wojiushikong@126.com>
2019-06-28 18:05:38 +0800

    temp add bitcoin checker

--- src/modules/bitcoin.spec.ts
+++ src/modules/bitcoin.spec.ts
@@ -3,6 +3,6 @@ import test from 'ava';
 import BitcoinChecker from './bitcoin';
 
 test('validate', t => {
-  t.is(BitcoinChecker.validate('test'), true);
+  t.is(BitcoinChecker.validate('12KYrjTdVGjFMtaxERSk3gphreJ5US8aUP'), true);
   t.is(BitcoinChecker.validate(''), false);
 });
--- src/modules/bitcoin.ts
+++ src/modules/bitcoin.ts
@@ -1,8 +1,38 @@
+import bs58 from 'bs58';
 import IChecker from '../interfaces/checker.interface';
+import { toHex } from '../utils';
 
 class BitcoinChecker implements IChecker {
+  protected EXPECTED_LENGTH: number = 25;
+  protected HASH_FUNCTION: string = 'sha256';
   public validate(address: string): boolean {
+    this.getAddressType(address);
     return address.length > 0;
+  }
+
+  protected isLegalAddress(address: string, decoded: Buffer): boolean {
+    if (address && decoded && decoded.length > 0) {
+      if (decoded.length === this.EXPECTED_LENGTH) {
+        return true;
+      }
+    }
+    return false;
+  }
+
+  protected getAddressType(address: string): string | null {
+    const decoded: Buffer = bs58.decode(address);
+    console.log('d', decoded);
+
+    if (this.isLegalAddress(address, decoded)) {
+      const decodedLength: number = decoded.length;
+
+      const addressChecksum: string = toHex(
+        decoded.slice(decodedLength - 4, decodedLength)
+      );
+      console.log('addressChecksum', addressChecksum);
+      return '';
+    }
+    return null;
   }
 }
 
