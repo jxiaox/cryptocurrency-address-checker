f40fa39694e50a8d592fe3e51cca6ed782865a23
jxiaox <wojiushikong@126.com>
2019-07-02 18:28:30 +0800

    bitcoin getAddressType

--- src/modules/bitcoin.ts
+++ src/modules/bitcoin.ts
@@ -1,13 +1,18 @@
-import bs58 from 'bs58';
-import IChecker from '../interfaces/checker.interface';
-import { toHex } from '../utils';
+import IChecker from '@/interfaces/checker.interface';
+import { bs58, sha256 } from '@/lib/hash';
+import { toHex } from '@/utils';
 
 class BitcoinChecker implements IChecker {
   protected EXPECTED_LENGTH: number = 25;
   protected HASH_FUNCTION: string = 'sha256';
   public validate(address: string): boolean {
-    this.getAddressType(address);
+    const addressType = this.getAddressType(address);
+    console.log('addressType', addressType);
+
     return address.length > 0;
+  }
+  protected getChecksum(bodyHex: string): string {
+    return sha256(sha256(bodyHex)).substr(0, 8);
   }
 
   protected isLegalAddress(address: string, decoded: Buffer): boolean {
@@ -19,18 +24,29 @@ class BitcoinChecker implements IChecker {
     return false;
   }
 
+  /**
+   * 获取地址类型
+   *
+   * @protected
+   * @param {string} address
+   * @returns {(string | null)}
+   * @memberof BitcoinChecker
+   */
   protected getAddressType(address: string): string | null {
     const decoded: Buffer = bs58.decode(address);
-    console.log('d', decoded);
 
     if (this.isLegalAddress(address, decoded)) {
       const decodedLength: number = decoded.length;
 
-      const addressChecksum: string = toHex(
+      const checksumHex: string = toHex(
         decoded.slice(decodedLength - 4, decodedLength)
       );
-      console.log('addressChecksum', addressChecksum);
-      return '';
+      const bodyHex: string = toHex(decoded.slice(0, decodedLength - 4));
+      const baseChecksum: string = this.getChecksum(bodyHex);
+      console.log('addressChecksum', checksumHex, bodyHex, baseChecksum);
+      if (checksumHex === baseChecksum) {
+        return toHex(decoded.slice(0, this.EXPECTED_LENGTH - 24));
+      }
     }
     return null;
   }
