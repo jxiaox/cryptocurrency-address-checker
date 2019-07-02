e0f4fbfb26879ce485cc7a60cd0b85b1090ee51f
jxiaox <wojiushikong@126.com>
2019-07-02 18:26:22 +0800

    add base58 to hash.ts

--- src/lib/hash.ts
+++ src/lib/hash.ts
@@ -1,4 +1,3 @@
-import { createHash } from 'crypto';
 import shaJs from 'sha.js';
 
 /**
@@ -19,20 +18,4 @@ export function sha256(message: string): string {
     .digest('hex');
 }
 
-/**
- * A faster implementation of [[sha256]] which requires the native Node.js module. Browser consumers should use [[sha256]], instead.
- *
- * ### Example (es imports)
- * ```js
- * import { sha256Native as sha256 } from 'typescript-starter'
- * sha256('test')
- * // => '9f86d081884c7d659a2feaa0c55ad015a3bf4f1b2b0b822cd15d6c15b0f00a08'
- * ```
- *
- * @returns sha256 message digest
- */
-export function sha256Native(message: string): string {
-  return createHash('sha256')
-    .update(message)
-    .digest('hex');
-}
+export import bs58 = require('bs58');
