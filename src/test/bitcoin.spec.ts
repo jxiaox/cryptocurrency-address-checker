58a06a882768f398955d06da6ae95712ae4cf93b
jxiaox <wojiushikong@126.com>
2019-07-04 13:54:03 +0800

    bitcoin unit test

--- src/test/bitcoin.spec.ts
+++ src/test/bitcoin.spec.ts
@@ -1,11 +1,16 @@
 import BitcoinChecker from '@/modules/bitcoin';
 
-test('Jest-TypeScript 尝试运行', () => {
+test('check address type', () => {
   // mainnet
   expect(
     BitcoinChecker.getAddressType('1PMycacnJaSqwwJqjawXBErnLsZ7RkXUAs')
-  ).toBe('00'); // Pass
-  // expect(
-  //   BitcoinChecker.getAddressType('mvppDXkpVQx6bNgAqKjkaFsH8FZAMM3gSC')
-  // ).toBe('00');
+  ).toBe('00');
+  expect(
+    BitcoinChecker.getAddressType('3NJZLcZEEYBpxYEUGewU4knsQRn1WM5Fkt')
+  ).toBe('05');
+
+  // testnet
+  expect(
+    BitcoinChecker.getAddressType('mvppDXkpVQx6bNgAqKjkaFsH8FZAMM3gSC')
+  ).toBe('6f');
 });
