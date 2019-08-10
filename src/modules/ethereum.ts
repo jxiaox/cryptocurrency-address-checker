1bbd04cb7de74235879e25e344cd3772d8037676
jxiaox <wojiushikong@126.com>
2019-08-10 22:24:23 +0800

    checksum address

--- src/modules/ethereum.ts
+++ src/modules/ethereum.ts
@@ -17,9 +17,51 @@ class EtherChecker implements IChecker, ICoin {
     this.symbol = coinsConfig.eth.symbol;
   }
 
+  /**
+   * Checks if the given string is an address
+   *
+   * @method validate
+   *
+   * @param {String} address the given HEX address
+   *
+   * @param {Number} chainId to define checksum behavior
+   *
+   * @returns {Boolean}
+   */
   public validate(address: string): boolean {
     // check address regex or checksum address
     return web3.utils.isAddress(address);
+  }
+
+  /**
+   * Checks if the given string is a checksummed address
+   *
+   * @method checkAddressChecksum
+   *
+   * @param {String} address the given HEX address
+   *
+   * @param {number} chain where checksummed address should be valid.
+   *
+   * @returns {Boolean}
+   */
+
+  public checkAddressChecksum(address: string): boolean {
+    return web3.utils.checkAddressChecksum(address);
+  }
+
+  /**
+   * Converts to a checksum address
+   *
+   * @method toChecksumAddress
+   *
+   * @param {string} address the given HEX address
+   *
+   * @param {number} chain where checksummed address should be valid.
+   *
+   * @returns {string} address with checksum applied.
+   */
+  public toChecksumAddress(address: string): string {
+    return web3.utils.toChecksumAddress(address);
   }
 }
 
